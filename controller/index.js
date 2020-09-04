2156c4eda8d8420c85066eeebbeb036b45fd6f8e
Tlezip <title0088@gmail.com>
2020-09-04 15:14:37 +0700

    add get user by id for user schema

--- controller/index.js
+++ controller/index.js
@@ -19,6 +19,23 @@ const addUser = ({ name, title, age }) => {
   })
 }
 
+const getUserById = async id => {
+  const sql = `
+    SELECT * FROM user WHERE id = "${id}"
+  `
+  const user = await new Promise((resolve, reject) => {
+    databaeConnection.query(sql, (err, data) => {
+      if (err) {
+        console.log('error', err)
+        reject({})
+      }
+      resolve(data.length > 0 ? data[0] : {})
+    })
+  })
+  return user
+}
+
 module.exports = {
-  addUser
+  addUser,
+  getUserById
 }
--- resolver/index.js
+++ resolver/index.js
@@ -1,8 +1,9 @@
-const { addUser } = require('../controller')
+const { addUser, getUserById } = require('../controller')
 
 const resolver = {
   hello: () => 'Hello World',
-  addUser: ({ name, title, age }) => addUser({ name, title, age })
+  addUser: ({ name, title, age }) => addUser({ name, title, age }),
+  user: ({ id }) => getUserById(id)
 }
 
 module.exports = resolver
